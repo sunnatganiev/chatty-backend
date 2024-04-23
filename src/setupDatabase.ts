fc34c677d7b9fc737c3a7e5f8755dd2105462598
Sunnatullo Ganiev <sunnatulloganiev@Sunnatullos-MacBook-Pro.local>
2024-04-23 20:16:20 +0100

    setup project

---
+++ src/setupDatabase.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import { config } from './config';
+import Logger from 'bunyan';
+
+const log: Logger = config.createLogger('setupDatabase');
+
+export default () => {
+  const connect = () => {
+    mongoose
+      .connect(`${config.DATABASE_URL}`)
+      .then(() => {
+        log.info('Successfully connected to database');
+      })
+      .catch((err) => {
+        log.error('Error connection to database', err);
+        return process.exit(1);
+      });
+  };
+
+  connect();
+
+  mongoose.connection.on('disconnected', connect);
+};
